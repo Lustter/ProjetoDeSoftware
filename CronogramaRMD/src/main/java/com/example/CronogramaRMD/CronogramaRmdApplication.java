508e761f26f4ca87fcd711ace0a257f5fbd91b47
Vitor Alexandre Lusni <lusnivitor@outlook.com>
2025-09-30 10:51:02 -0300

    .
    .

---
+++ CronogramaRMD/src/main/java/com/example/CronogramaRMD/CronogramaRmdApplication.java
@@ -0,0 +1,13 @@
+package com.example.CronogramaRMD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronogramaRmdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CronogramaRmdApplication.class, args);
+	}
+
+}
